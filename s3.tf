5cc9ac9553b51dd05f666a04abb62e3cba2e6c39
nandeesh-gajula <nandeesh.gajula@contentstack.com>
2022-05-04 12:09:22 +0530

    add s3

--- s3.tf
+++ s3.tf
@@ -2,32 +2,32 @@ resource "aws_s3_bucket" "today" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "today_acl" {
-  bucket = aws_s3_bucket.today.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "today" {
-  bucket = aws_s3_bucket.today.id
-
-  block_public_acls   = true
-  block_public_policy = true
-}
-
-resource "aws_s3_bucket_versioning" "versioning_today" {
-  bucket = aws_s3_bucket.today.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "today" {
-  bucket = aws_s3_bucket.today.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = var.sse_enable
-    }
-  }
-}
-
+#resource "aws_s3_bucket_acl" "today_acl" {
+#  bucket = aws_s3_bucket.today.id
+#  acl    = "private"
+#}
+#
+#resource "aws_s3_bucket_public_access_block" "today" {
+#  bucket = aws_s3_bucket.today.id
+#
+#  block_public_acls   = true
+#  block_public_policy = true
+#}
+#
+#resource "aws_s3_bucket_versioning" "versioning_today" {
+#  bucket = aws_s3_bucket.today.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
+#
+#resource "aws_s3_bucket_server_side_encryption_configuration" "today" {
+#  bucket = aws_s3_bucket.today.bucket
+#
+#  rule {
+#    apply_server_side_encryption_by_default {
+#      sse_algorithm = var.sse_enable
+#    }
+#  }
+#}
+#
